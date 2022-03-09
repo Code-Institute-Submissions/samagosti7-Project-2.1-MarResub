e2d20aab30d31b3fe40d123a74508d574b7613d0
samagosti7 <sagosti7@gmail.com>
2022-03-09 19:51:14 +0000

    Add more text to showOutcome div displaying player and computer choice. Style nav header

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,31 +8,50 @@ let result= "";
 // 0=rock, 1=paper, 3=scissors
 // Function to increase score after the winner is decided
 
-function incrementScore(){
+function incrementScore(playerChoiceText, computerChoiceText){
     let oldScorePlayer = parseInt(document.getElementById("playerScore").innerText);
     let oldScoreComputer = parseInt(document.getElementById("computerScore").innerText)
     if(result=="win"){
         document.getElementById("playerScore").innerText = ++oldScorePlayer;
-        document.getElementById("showOutcome").innerText = "You won!";
+        document.getElementById("showOutcome").innerText = "You won! You chose " + playerChoiceText + ", computer chose " + computerChoiceText + ". ";
     }else if(result=="loss"){
         document.getElementById("computerScore").innerText = ++oldScoreComputer;
-        document.getElementById("showOutcome").innerText = "Computer Won.";
+        document.getElementById("showOutcome").innerText = "Computer Won. You chose " + playerChoiceText + ", computer chose " + computerChoiceText + ". ";
     }else{
-        document.getElementById("showOutcome").innerText = "Draw.";
+        document.getElementById("showOutcome").innerText = "Draw. You chose " + playerChoiceText + ", computer chose " + computerChoiceText + ". ";
     }
+    evaluateWinner();
+}
+
+function evaluateWinner(){
+    let playerScore1 = parseInt(document.getElementById("playerScore").innerText);
+    let computerScore1 = parseInt(document.getElementById("computerScore").innerText);
+    if(playerScore1>5){
+        alert("Player Has Won");
+        window.location.reload();
+    }
+    if(computerScore1>5){
+        alert("Computer Has Won");
+        window.location.reload();
+    }
+    
 }
 
 // Function finding winner if the user chooses rock
 function findWinnerRock(){
     computerChoice=Math.floor(Math.random()*3);
+    let computerChoiceText = "rock";
     if(computerChoice==0){
         result = "draw";
     }else if( computerChoice==1){
         result="win";
+        computerChoiceText="paper";
     }else{
         result="loss";
+        computerChoiceText="scissors"
     }
-    incrementScore();
+    // 0=rock, 1=paper, 3=scissors
+    incrementScore("rock",computerChoiceText);
 }
 
 // Event listener for the rock button
@@ -41,14 +60,17 @@ rockChoice.addEventListener("click", findWinnerRock);
 // Function finding winner if the user chooses paper
 function findWinnerPaper(){
     computerChoice=Math.floor(Math.random()*3);
+    let computerChoiceText = "rock";
     if(computerChoice==0){
         result = "win";
     }else if( computerChoice==1){
         result="draw";
+        computerChoiceText="paper";
     }else{
         result="loss";
+        computerChoiceText="scissors";
     }
-    incrementScore();
+    incrementScore("paper", computerChoiceText);
 }
 
 // Event listener for the paper button
@@ -57,14 +79,17 @@ paperChoice.addEventListener("click", findWinnerPaper);
 // Function finding winner if the user chooses scissors
 function findWinnerScissors(){
     computerChoice=Math.floor(Math.random()*3);
+    let computerChoiceText = "rock";
     if(computerChoice==0){
         result = "loss";
     }else if( computerChoice==1){
         result="win";
+        computerChoiceText="paper";
     }else{
-        result="draw"; 
+        result="draw";
+        computerChoiceText="scissors"; 
     }
-    incrementScore();
+    incrementScore("scissors", computerChoiceText);
 }
 
 // Event listener for the scissors button
